23f85c3cedb5b032721543eae1e27dc5229a029e
marsyuma <bintang.marsyuma@ui.ac.id>
2023-12-01 18:17:08 +0700

    Nambahin obat-obatan

--- lib/page/UserHome.dart
+++ lib/page/UserHome.dart
@@ -300,6 +300,16 @@ class UserHomePage extends StatelessWidget {
               ),
             ),
           ),
+          MedicationWidget(
+            medicationName: 'Medication 1',
+            medicationDescription: 'Description for Medication 1.',
+            imagePath: 'assets/icons/Bell.svg', // Adjust the path accordingly
+          ),
+          MedicationWidget(
+            medicationName: 'Medication 2',
+            medicationDescription: 'Description for Medication 2.',
+            imagePath: 'assets/icons/Bell.svg', // Adjust the path accordingly
+          ),
           const SafeArea(
             child: Column(
               children: [
@@ -347,5 +357,77 @@ class UserHomePage extends StatelessWidget {
       'Dec'
     ];
     return months[month - 1];
+  }
+}
+
+class MedicationWidget extends StatelessWidget {
+  final String medicationName;
+  final String medicationDescription;
+  final String imagePath;
+
+  MedicationWidget({
+    required this.medicationName,
+    required this.medicationDescription,
+    required this.imagePath,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.symmetric(vertical: 10, horizontal: 16),
+      padding: EdgeInsets.all(16),
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(10),
+        boxShadow: [
+          BoxShadow(
+            color: Colors.grey.withOpacity(0.5),
+            spreadRadius: 1,
+            blurRadius: 4,
+            offset: Offset(0, 2), // changes position of shadow
+          ),
+        ],
+      ),
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          // Medication Image
+          Container(
+            margin: EdgeInsets.only(right: 16),
+            width: 80,
+            height: 80,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(10),
+              image: DecorationImage(
+                image: AssetImage(imagePath),
+                fit: BoxFit.cover,
+              ),
+            ),
+          ),
+          // Medication Details
+          Expanded(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  medicationName,
+                  style: TextStyle(
+                    fontSize: 18,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                SizedBox(height: 8),
+                Text(
+                  medicationDescription,
+                  style: TextStyle(
+                    fontSize: 16,
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
